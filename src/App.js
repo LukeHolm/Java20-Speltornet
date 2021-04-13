ac8cba97e8dbb237d8814b0789ee41919255e607
Miranda <miranda.friso@stud.sti.se>
2021-04-13 21:20:13 +0200

    added body-bottom

--- src/App.js
+++ src/App.js
@@ -16,17 +16,17 @@ function App() {
               <button className="knapp" id="knapp1">Se spel</button>
             </div>
             <div className="col">
-            <img className="bild" src="https://anima-uploads.s3.amazonaws.com/projects/6074664a17d10c4e519008cf/releases/60746a0533c6fe6654c155b4/img/ska-rmavbild-2021-03-22-kl--15-31-6@2x.png" alt="bild" />
+              <img className="bild" src="https://anima-uploads.s3.amazonaws.com/projects/6074664a17d10c4e519008cf/releases/60746a0533c6fe6654c155b4/img/ska-rmavbild-2021-03-22-kl--15-31-6@2x.png" alt="bild" />
               <p className="spelnamn">Pandemic</p>
               <button className="knapp" id="knapp1">Se spel</button>
             </div>
             <div className="col">
-            <img className="bild" src="https://anima-uploads.s3.amazonaws.com/projects/6074664a17d10c4e519008cf/releases/60746a0533c6fe6654c155b4/img/ska-rmavbild-2021-03-22-kl--15-31-6@2x.png" alt="bild" />
+              <img className="bild" src="https://anima-uploads.s3.amazonaws.com/projects/6074664a17d10c4e519008cf/releases/60746a0533c6fe6654c155b4/img/ska-rmavbild-2021-03-22-kl--15-31-6@2x.png" alt="bild" />
               <p className="spelnamn">Pandemic</p>
               <button className="knapp" id="knapp1">Se spel</button>
             </div>
             <div className="col">
-            <img className="bild" src="https://anima-uploads.s3.amazonaws.com/projects/6074664a17d10c4e519008cf/releases/60746a0533c6fe6654c155b4/img/ska-rmavbild-2021-03-22-kl--15-31-6@2x.png" alt="bild" />
+              <img className="bild" src="https://anima-uploads.s3.amazonaws.com/projects/6074664a17d10c4e519008cf/releases/60746a0533c6fe6654c155b4/img/ska-rmavbild-2021-03-22-kl--15-31-6@2x.png" alt="bild" />
               <p className="spelnamn">Pandemic</p>
               <button className="knapp" id="knapp1">Se spel</button>
             </div>
@@ -40,19 +40,35 @@ function App() {
               <button className="knapp" id="knapp1">Se spel</button>
             </div>
             <div className="col">
-            <img className="bild" src="https://anima-uploads.s3.amazonaws.com/projects/6074664a17d10c4e519008cf/releases/60746a0533c6fe6654c155b4/img/ska-rmavbild-2021-03-22-kl--15-31-6@2x.png" alt="bild" />
+              <img className="bild" src="https://anima-uploads.s3.amazonaws.com/projects/6074664a17d10c4e519008cf/releases/60746a0533c6fe6654c155b4/img/ska-rmavbild-2021-03-22-kl--15-31-6@2x.png" alt="bild" />
               <p className="spelnamn">Pandemic</p>
               <button className="knapp" id="knapp1">Se spel</button>
             </div>
             <div className="col">
-            <img className="bild" src="https://anima-uploads.s3.amazonaws.com/projects/6074664a17d10c4e519008cf/releases/60746a0533c6fe6654c155b4/img/ska-rmavbild-2021-03-22-kl--15-31-6@2x.png" alt="bild" />
+              <img className="bild" src="https://anima-uploads.s3.amazonaws.com/projects/6074664a17d10c4e519008cf/releases/60746a0533c6fe6654c155b4/img/ska-rmavbild-2021-03-22-kl--15-31-6@2x.png" alt="bild" />
               <p className="spelnamn">Pandemic</p>
               <button className="knapp" id="knapp1">Se spel</button>
             </div>
             <div className="col">
-            <img className="bild" src="https://anima-uploads.s3.amazonaws.com/projects/6074664a17d10c4e519008cf/releases/60746a0533c6fe6654c155b4/img/ska-rmavbild-2021-03-22-kl--15-31-6@2x.png" alt="bild" />
+              <img className="bild" src="https://anima-uploads.s3.amazonaws.com/projects/6074664a17d10c4e519008cf/releases/60746a0533c6fe6654c155b4/img/ska-rmavbild-2021-03-22-kl--15-31-6@2x.png" alt="bild" />
               <p className="spelnamn">Pandemic</p>
               <button className="knapp" id="knapp1">Se spel</button>
+            </div>
+          </div>
+        </div>
+        <div className="body-bottom">
+          <h4 className="explanation">Hur funkar det?</h4>
+          <div class="container">
+            <div class="row">
+              <div class="col">
+                1
+              </div>
+              <div class="col">
+                2
+              </div>
+              <div class="col">
+                3
+              </div>
             </div>
           </div>
         </div>
