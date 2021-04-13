bca77d8a00a3017fed4b2bf9278c02728acad81d
Miranda <miranda.friso@stud.sti.se>
2021-04-13 13:27:02 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ function App() {
       <header className="App-header">
         <div class="top-bar"></div>
       </header>
+      <footer className="App-footer">
+        <div class="row align-items-center">
+          <div class="col">
+            One of three columns
+    </div>
+          <div class="col">
+            One of three columns
+    </div>
+          <div class="col">
+            One of three columns
+    </div>
+        </div>
+      </footer>
     </div>
   );
 }
