8fb38aad7ea2837d7764d60330e6aab19a563fdd
Lahey <oskar.franck@gmail.com>
2021-04-13 14:48:41 +0200

    landinpage continue

--- src/App.js
+++ src/App.js
@@ -2,40 +2,70 @@ import './App.css';
 
 function App() {
   return (
-    <div className="Ap">
-      <header className="App-heade">
-        <div class="top-bar"></div>
-        <div class="top-bar-overlap"></div>
+    <div className="App">
+      <header className="App-header">
+        <div className="top-bar"></div>
+        <div className="top-bar-overlap"></div>
       </header>
       <body>
-        <div class="container">
-          <div class="row">
-            <div class="col-sm">
-              <img class="bild" src="https://anima-uploads.s3.amazonaws.com/projects/6074664a17d10c4e519008cf/releases/60746a0533c6fe6654c155b4/img/ska-rmavbild-2021-03-22-kl--15-31-6@2x.png" alt="bild" />
-              <p class="spelnamn">Pandemic</p>
-              <button class="knapp" id="knapp1" />
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <img className="bild" src="https://anima-uploads.s3.amazonaws.com/projects/6074664a17d10c4e519008cf/releases/60746a0533c6fe6654c155b4/img/ska-rmavbild-2021-03-22-kl--15-31-6@2x.png" alt="bild" />
+              <p className="spelnamn">Pandemic</p>
+              <button className="knapp" id="knapp1">Se spel</button>
             </div>
-            <div class="col-sm">
-              One of three columns
+            <div className="col">
+            <img className="bild" src="https://anima-uploads.s3.amazonaws.com/projects/6074664a17d10c4e519008cf/releases/60746a0533c6fe6654c155b4/img/ska-rmavbild-2021-03-22-kl--15-31-6@2x.png" alt="bild" />
+              <p className="spelnamn">Pandemic</p>
+              <button className="knapp" id="knapp1">Se spel</button>
             </div>
-            <div class="col-sm">
-              One of three columns
+            <div className="col">
+            <img className="bild" src="https://anima-uploads.s3.amazonaws.com/projects/6074664a17d10c4e519008cf/releases/60746a0533c6fe6654c155b4/img/ska-rmavbild-2021-03-22-kl--15-31-6@2x.png" alt="bild" />
+              <p className="spelnamn">Pandemic</p>
+              <button className="knapp" id="knapp1">Se spel</button>
             </div>
-            <div class="col-sm">
-              One of three columns
+            <div className="col">
+            <img className="bild" src="https://anima-uploads.s3.amazonaws.com/projects/6074664a17d10c4e519008cf/releases/60746a0533c6fe6654c155b4/img/ska-rmavbild-2021-03-22-kl--15-31-6@2x.png" alt="bild" />
+              <p className="spelnamn">Pandemic</p>
+              <button className="knapp" id="knapp1">Se spel</button>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <img className="bild" src="https://anima-uploads.s3.amazonaws.com/projects/6074664a17d10c4e519008cf/releases/60746a0533c6fe6654c155b4/img/ska-rmavbild-2021-03-22-kl--15-31-6@2x.png" alt="bild" />
+              <p className="spelnamn">Pandemic</p>
+              <button className="knapp" id="knapp1">Se spel</button>
+            </div>
+            <div className="col">
+            <img className="bild" src="https://anima-uploads.s3.amazonaws.com/projects/6074664a17d10c4e519008cf/releases/60746a0533c6fe6654c155b4/img/ska-rmavbild-2021-03-22-kl--15-31-6@2x.png" alt="bild" />
+              <p className="spelnamn">Pandemic</p>
+              <button className="knapp" id="knapp1">Se spel</button>
+            </div>
+            <div className="col">
+            <img className="bild" src="https://anima-uploads.s3.amazonaws.com/projects/6074664a17d10c4e519008cf/releases/60746a0533c6fe6654c155b4/img/ska-rmavbild-2021-03-22-kl--15-31-6@2x.png" alt="bild" />
+              <p className="spelnamn">Pandemic</p>
+              <button className="knapp" id="knapp1">Se spel</button>
+            </div>
+            <div className="col">
+            <img className="bild" src="https://anima-uploads.s3.amazonaws.com/projects/6074664a17d10c4e519008cf/releases/60746a0533c6fe6654c155b4/img/ska-rmavbild-2021-03-22-kl--15-31-6@2x.png" alt="bild" />
+              <p className="spelnamn">Pandemic</p>
+              <button className="knapp" id="knapp1">Se spel</button>
             </div>
           </div>
         </div>
       </body>
       <footer className="App-footer">
-        <div class="row align-items-center">
-          <div class="col">
+        <div className="row align-items-center">
+          <div className="col">
             One of three columns
     </div>
-          <div class="col">
+          <div className="col">
             One of three columns
     </div>
-          <div class="col">
+          <div className="col">
             One of three columns
     </div>
         </div>
