881dc9ab552899ca5f99ae07533a794402b5ef8c
Miranda <miranda.friso@stud.sti.se>
2021-04-21 09:57:51 +0200

    form package

---
+++ src/components/Form.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { useForm } from "react-hook-form";
+
+/*{One of the key concepts in React Hook Form is to register your uncontrolled component 
+    into the hook. This will make its value available for both the form validation and submission.}*/
+
+    
