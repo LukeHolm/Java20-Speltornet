21a1d5611d2e515357f8147de98d2be7561f4303
Lucas Holm <lucas.e.holm@gmail.com>
2021-04-21 10:18:21 +0200

    Updated pathways in App.js and Gamecard.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
 import './App.css';
-import Burger from './components/Burger';
-import Catan from './components/Catan';
-import Pandemic from './components/Pandemic';
-import GamePopup from './components/GamePopup';
-import {React, useState} from 'react';
-import ModalButton from './components/ModalButton';
 import React from 'react';
 import Landingpage from './components/Landingpage';
 
--- src/components/Gamecard.js
+++ src/components/Gamecard.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import GamePopup from './GamePopup';
+import ModalButton from './ModalButton'
 
 
 
 const Gamecard = () => {
 
     const [gamecards, setGamecards] = useState([]);
-    const [show, setShow] = useState(false);
 
     useEffect(() => {
         fetch("gamecatalog.json").then(response => response.json().then(data => setGamecards(data)))
@@ -18,10 +17,7 @@ const Gamecard = () => {
                 <div className="box col">
                     <img className="bild" src={gamecard.imageURL} alt="bild" />
                     <p className="spelnamn">{gamecard.title}</p>
-                    <button className="knapp" onClick={() => setShow(true)}>Se spel</button>
-                    <GamePopup title={gamecard.title} onClose={() => setShow(false)} show={show}>
-                        <p>Body body!</p>
-                    </GamePopup>
+                    <ModalButton />
                 </div>
             ))}
         </div>
