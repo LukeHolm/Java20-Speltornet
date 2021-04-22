4adb940968f99e83842df81764a99e283341776c
Miranda <miranda.friso@stud.sti.se>
2021-04-22 21:02:35 +0200

    camera component and form

---
+++ src/components/Camera.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const camera = () => {
+    return (
+        <div className="camera">
+            <i class="far fa-camera"></i>
+        </div>
+    )
+}
+
+export default camera;
--- src/components/Form.js
+++ src/components/Form.js
@@ -24,33 +24,40 @@ const Form = () => {
             </>
             <div>
                 <h1 className="form-h1">Bytesförfrågan</h1>
+            </div>
+            <div className="form-element">
+                <h2 className="form-h2">Du vill ha:</h2>
+            </div>
+            <div className="form-element">
                 <h2 className="form-h2">Vilket spel vill du byta bort?</h2>
                 <label className="form-h3" for="boardgame">Spelets namn: </label><br></br>
-                <input type="text" id="boardgame" value={boardgame} onChange={(event) => setBoardgame(event.target.value)} />
+                <input className="thin-textarea" type="text" id="boardgame" placeholder="Skriv här" 
+                value={boardgame} onChange={(event) => setBoardgame(event.target.value)} />
             </div>
-            <div>
+            <div className="form-element">
                 <h3 className="form-h3">Ladda upp bilder på ditt spel:</h3>
-                <i class="far fa-camera"></i>
+                <div className="camera-box"></div>
             </div>
+
             <hr className="line"></hr>
-            <div>
+            <div className="form-element">
                 <h3 className="form-h3">Skick på ditt spel:</h3>
                 <h3 className="form-h3">Slitage:</h3>
-                <input type="radio" name="gameCondition" value="new" />
-                <label for="new">Nyskick</label>
-                <input type="radio" name="gameCondition" value="littleWorn" />
-                <label for="littleWorn">Lite slitet</label>
-                <input type="radio" name="gameCondition" value="muchWorn" />
-                <label for="muchWorn">Mycket slitet</label>
+                <input type="radio" id="radio" name="gameCondition" value="new" />
+                <label for="new" id="radio-text">Nyskick</label>
+                <input type="radio" id="radio" name="gameCondition" value="littleWorn" />
+                <label for="littleWorn" id="radio-text">Lite slitet</label>
+                <input type="radio" id="radio" name="gameCondition" value="muchWorn" />
+                <label for="muchWorn" id="radio-text">Mycket slitet</label>
             </div>
-            <div>
+            <div className="form-element">
                 <h3 className="form-h3">Komponenter:</h3>
-                <input type="radio" name="gameComponents" value="new" />
-                <label for="new">Ingen del saknas</label>
-                <input type="radio" name="gameComponents" value="littleWorn" />
-                <label for="littleWorn">Delar saknas</label>
+                <input type="radio" id="radio" name="gameComponents" value="new" />
+                <label for="allParts" id="radio-text">Ingen del saknas</label>
+                <input type="radio" id="radio" name="gameComponents" value="littleWorn" />
+                <label for="partsMissing" id="radio-text">Delar saknas</label>
             </div>
-            <div>
+            <div className="form-element">
                 <h3 className="form-h3">Detaljer</h3>
                 <label for="details" className="body-text">Saknade komponenter och slitage</label>
                 <textarea className="textarea" name="details" rows="5" cols="30" placeholder="Skriv här">
@@ -58,7 +65,7 @@ const Form = () => {
             </div>
             <hr className="line"></hr>
 
-            <div>
+            <div className="form-element">
                 <h3 className="form-h3">Hälsning</h3>
                 <label for="greeting" className="body-text">Skriv en kort hälsning till bytaren! </label>
                 <textarea className="textarea" name="greeting" rows="5" cols="30" placeholder="Skriv här">
@@ -66,16 +73,16 @@ const Form = () => {
             </div>
 
             <hr className="line"></hr>
-            <div>
-                <h3 className="form-h3">Välj leveranssätt::</h3>
-                <input type="radio" name="gameCondition" value="new" />
-                <label for="new">Postnord - Hemleverans     (46 kr)</label><br></br>
-                <input type="radio" name="gameCondition" value="littleWorn" />
-                <label for="littleWorn">Instabox - Närmaste återbud     (0 kr)</label><br></br>
-                <input type="radio" name="gameCondition" value="muchWorn" />
-                <label for="muchWorn">Brevuggla - Hemleverans     (Lite vatten)</label><br></br>
+            <div className="form-element">
+                <h3 className="form-h3">Välj leveranssätt:</h3>
+                <input type="radio" id="radio" name="delivery" value="new" />
+                <label for="homeDelivery">Postnord - Hemleverans     (46 kr)</label><br></br>
+                <input type="radio" id="radio" name="delivery" value="littleWorn" />
+                <label for="pickupDelivery">Instabox - Närmaste återbud     (0 kr)</label><br></br>
+                <input type="radio" id="radio" name="delivery" value="muchWorn" />
+                <label for="owlDelivery">Brevuggla - Hemleverans     (Lite vatten)</label><br></br>
             </div>
-            <div>
+            <div className="form-element">
                 <input type="submit" id="submitbutton" value="Send" />
             </div>
             <>
