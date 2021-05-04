0eef1e4b1f4e317e26c021117517734bf76c5d2d
Lahey <oskar.franck@gmail.com>
2021-05-04 15:03:56 +0200

    loaddata added

---
+++ src/components/LoadData.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+
+
+const LoadData = () => {
+
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        fetch("gamesavailable.json").then(response => response.json().then(data => setGames(data)))
+    }, [])
+
+    const filterGames = (arr, query) => {
+        return arr.filter(el => el.title.toLowerCase().indexOf(query.toLowerCase()) !== -1).map(game =>
+            <>
+            <p>{game.title}</p>
+            <p>{game.condition}</p>
+            <p>{game.parts}</p>
+            <img className="bild" src={game.imageURL} alt={game.title}/>
+            </>
+            )
+    }
+
+    return (
+        <>
+        {filterGames(games, "Pandemic")}
+        </>
+    )
+}
+
+export default LoadData;
