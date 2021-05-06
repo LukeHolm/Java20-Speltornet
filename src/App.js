37e07d6b193a4220a0db56a728f61ba7ce7b1230
Lucas Holm <lucas.e.holm@gmail.com>
2021-05-06 14:52:58 +0200

    Updated rout to gameAdds

--- src/App.js
+++ src/App.js
@@ -20,9 +20,7 @@ function App() {
           <Route exact path="/" component={Landingpage} />
           <Route path="/CreateAdd" component={CreateAdd} />
           <Route path="/detail" component={Details} />
-          <Route exact path="/GameAdds/:gameId">
-            <GameAdds id={gamecards.id} title={gamecards.title} imageURL={gamecards.imageURL} description={gamecards.description} />
-          </Route>
+          <Route exact path="/GameAdds/:gameId" component={GameAdds}/>
         </Switch>
       </div>
     </Router>
