d83391fc697c11e808a3abbef07caaba7a6bcc64
Lucas Holm <lucas.e.holm@gmail.com>
2021-05-26 13:34:13 +0200

    Removed old data from previous modal

--- src/components/CancelButton.js
+++ src/components/CancelButton.js
@@ -2,13 +2,13 @@ import {React, useState} from 'react';
 import CancelPopup from './CancelPopup';
 
 
-const CancelButton = gamecard => {
+const CancelButton = () => {
     const [show, setShow] = useState(false)
 
     return (
         <div>
             <button className="cancel" onClick={() => setShow(true)}>Avbryt</button>
-            <CancelPopup onClose={() => setShow(false)} show={show} title={gamecard.title} imageURL={gamecard.imageURL} description={gamecard.description}>
+            <CancelPopup onClose={() => setShow(false)} show={show}>
             </CancelPopup>
         </div>
     )
